3d6ec626e620fc1b728e571b884c251e9e711466
rashiccv <32409006+merashi@users.noreply.github.com>
2019-04-10 10:49:56 +0530

    declare protected var:(int data,Node leftchild,Node rightchild)

---
+++ src/main/Tree.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Tree {
+    protected static class Node {
+        protected int data;
+        protected Node leftchild;
+        protected Node rightchild;
+
+    }
+}
