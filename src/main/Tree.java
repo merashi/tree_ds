753a9bf1fd3301647e1b67c106a4d5d3ca2bf5ae
rashiccv <32409006+merashi@users.noreply.github.com>
2019-04-17 10:19:42 +0530

    commit all

--- src/main/Tree.java
+++ src/main/Tree.java
@@ -19,5 +19,70 @@ public class Tree {
     protected Tree(Node root){
         this.root = root;
     }
+    public Tree(int data, Tree leftTree, Tree rightTree){
+        this.root = new Node(data);
+        if(leftTree != null){
+            this.root.leftchild = leftTree.root;
+        }
+        else {
+            this.root.leftchild = null;
+        }
+        if(rightTree != null){
+            this.root.rightchild = rightTree.root;
+        }
+        else {
+            this.root.rightchild = null;
+        }
+    }
+    public Tree getLeftSubtree() {
+        if (root != null && root.leftchild != null) {
+            return new Tree(root.leftchild);
+        } else {
+            return null;
+        }
+    }
 
+    public Tree getRightSubtree() {
+        if (root != null && root.rightchild != null) {
+            return new Tree(root.rightchild);
+        } else {
+            return null;
+        }
+    }
+
+    public boolean isLeaf(){
+        return (root.leftchild == null && root.rightchild == null);
+    }
+
+    public boolean isLeaf(Node node){
+        return (node.leftchild == null && node.rightchild == null);
+    }
+
+    private boolean isNullTree(){
+        boolean response = false;
+        if(this.root == null){
+            response = true;
+        }
+        return response;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        toString(root,1,sb);
+        return sb.toString();
+    }
+    private void toString(Node node, int depth, StringBuilder stringBuilder){
+        for (int i = 0; i < depth; i++) {
+            stringBuilder.append(" - - ");
+        }
+        if(node == null){
+            stringBuilder.append("null\n");
+        }
+        else{
+            stringBuilder.append(node.toString());
+            stringBuilder.append("\n");
+            toString(node.leftchild, depth+1, stringBuilder);
+        }
+    }
 }
