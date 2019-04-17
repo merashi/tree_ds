53bcbaef91cd841a3f6e15c2e510eebc2b834df6
rashiccv <32409006+merashi@users.noreply.github.com>
2019-04-17 10:12:02 +0530

    create static class Node

--- src/main/Tree.java
+++ src/main/Tree.java
@@ -1,5 +1,16 @@
 package main;
 
 public class Tree {
+    protected static class Node {
+        protected int data;
+        protected Node leftchild;
+        protected Node rightchild;
+
+        public Node(int data){
+            this.data = data;
+            this.leftchild = null;
+            this.rightchild = null;
+        }
+    }
 
 }
