69b5bb026112d0cc66efc318c2d11191a38ab7c7
rashiccv <32409006+merashi@users.noreply.github.com>
2019-04-17 10:10:40 +0530

    create class tree

--- src/main/Tree.java
+++ src/main/Tree.java
@@ -1,10 +1,5 @@
 package main;
 
 public class Tree {
-    protected static class Node {
-        protected int data;
-        protected Node leftchild;
-        protected Node rightchild;
 
-    }
 }
