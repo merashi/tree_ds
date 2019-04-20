1124859002b069b69036b423d08cb78425dee1ca
rashiccv <32409006+merashi@users.noreply.github.com>
2019-04-20 14:20:30 +0530

    create method BubbleSort

--- src/main/BubbleSort.java
+++ src/main/BubbleSort.java
@@ -2,4 +2,12 @@ package main;
 
 public class BubbleSort {
     private long[] array;
+    private int numElements;
+    public BubbleSort(int n){
+        array = new long[n];
+        numElements = 0;
+    }
+
+
+
 }
