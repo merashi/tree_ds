cc77e97f2677a5d4af7254cda15c6fc4427586ce
rashiccv <32409006+merashi@users.noreply.github.com>
2019-04-20 14:52:01 +0530

    create method display

--- src/main/SelectionSort.java
+++ src/main/SelectionSort.java
@@ -11,5 +11,11 @@ public class SelectionSort {
         array[numElements] = value;
         numElements++;
     }
+    public void display() {
+        for (int i = 0; i < numElements; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println("");
+    }
 
 }
