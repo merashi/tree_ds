e04b671810c9b3b50e48223ed1ba0f5f181e1c68
rashiccv <32409006+merashi@users.noreply.github.com>
2019-04-20 14:50:48 +0530

    create constructor SelectionSort

--- src/main/SelectionSort.java
+++ src/main/SelectionSort.java
@@ -3,5 +3,9 @@ package main;
 public class SelectionSort {
     private long[] array;
     private int numElements;
+    public SelectionSort(int n){
+        array = new long[n];
+        numElements = 0;
+    }
 
 }
